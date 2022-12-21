84c7a468bd2187fe638547f9e8a4793050c3cce9
NicolasBuffa <nicolas.buffa@cencosud.com>
2022-12-21 15:10:03 -0300

    config button

--- tareaNicolasBuffa/tareaNicolasBuffa/MyCustomTableViewCell.swift
+++ tareaNicolasBuffa/tareaNicolasBuffa/MyCustomTableViewCell.swift
@@ -33,31 +33,85 @@ class MyCustomTableViewCell: UITableViewCell {
 
         configFontSize ()
         configColor ()
-
-        
-        propertyColor.text = "Color:"
-        propertySize.text = "Talle:"
+        configTextDefault ()
+        configBtn()
+   
+ 
         
 
         
     }
-    
     func configFontSize (){
-        nameProduct.font = UIFont.boldSystemFont(ofSize: 14)
-        discountProduct.font = UIFont.boldSystemFont(ofSize: 15)
-        priceProduct.font = UIFont.systemFont(ofSize: 20)
-        oldPriceProduct.font = UIFont.systemFont(ofSize: 15)
-        propertyColor.font = UIFont.systemFont(ofSize: 14)
-        colorProduct.font = UIFont.systemFont(ofSize: 14)
-        propertySize.font = UIFont.systemFont(ofSize: 14)
-        sizeProduct.font = UIFont.systemFont(ofSize: 14)
-    }
-    
-    func configColor (){
-        discountProduct.textColor = UIColor(red: 0.07, green: 0.45, blue: 0.87, alpha: 1.00)
-        priceProduct.textColor = .darkGray
-        oldPriceProduct.textColor = .lightGray
-        message.backgroundColor = UIColor(red: 0.91, green: 0.75, blue: 0.41, alpha: 0.80)
-    }
+
+           nameProduct.font = UIFont.boldSystemFont(ofSize: 14)
+
+           discountProduct.font = UIFont.boldSystemFont(ofSize: 15)
+
+           priceProduct.font = UIFont.systemFont(ofSize: 20)
+
+           oldPriceProduct.font = UIFont.systemFont(ofSize: 15)
+
+           propertyColor.font = UIFont.systemFont(ofSize: 14)
+
+           colorProduct.font = UIFont.systemFont(ofSize: 14)
+
+           propertySize.font = UIFont.systemFont(ofSize: 14)
+
+           sizeProduct.font = UIFont.systemFont(ofSize: 14)
+
+           message.font = UIFont.boldSystemFont(ofSize: 15)
+
+       }
+
+       
+
+       func configColor (){
+
+           discountProduct.textColor = UIColor(red: 0.07, green: 0.45, blue: 0.87, alpha: 1.00)
+
+           priceProduct.textColor = UIColor(red: 0.43, green: 0.44, blue: 0.44, alpha: 1.00)
+
+           oldPriceProduct.textColor = .lightGray
+
+           message.backgroundColor = UIColor(red: 1.00, green: 0.77, blue: 0.42, alpha: 0.50)
+
+           message.textColor = UIColor(red: 0.96, green: 0.53, blue: 0.20, alpha: 1.00)
+
+           propertySize.textColor = UIColor(red: 0.43, green: 0.44, blue: 0.44, alpha: 1.00)
+
+           colorProduct.textColor =  UIColor(red: 0.43, green: 0.44, blue: 0.44, alpha: 1.00)
+
+           propertyColor.textColor = UIColor(red: 0.43, green: 0.44, blue: 0.44, alpha: 1.00)
+
+           sizeProduct.textColor = UIColor(red: 0.43, green: 0.44, blue: 0.44, alpha: 1.00)
+
+       }
+
+       
+
+       func configTextDefault (){
+
+           propertyColor.text = "Color:"
+
+           propertySize.text = "Talle:"
+
+           message.text = "Últimas unidades"
+     
+
+       }
+
+       
+
+       func configBtn (){
+
+           btnAddCart.backgroundColor = .blue
+
+           btnAddCart.setTitleColor(.white, for: .normal)
+
+           btnAddCart.setTitle("Agregar a mi Carrito", for: .normal)
+
+           btnAddCart.layer.cornerRadius = 18.0
+
+       }
 
 }
