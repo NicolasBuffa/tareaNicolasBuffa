b3ca1b33bef73f5492fad0a2a2051886fe0fb6fd
NicolasBuffa <nicolas.buffa@cencosud.com>
2022-12-29 14:15:47 -0300

    UI MDP

--- tareaNicolasBuffa/tareaNicolasBuffa/Modules/ProductDetail/Views/ViewControllerMDP.swift
+++ tareaNicolasBuffa/tareaNicolasBuffa/Modules/ProductDetail/Views/ViewControllerMDP.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewControllerMDP: UIViewController {
   var product: Product?
-    override func viewDidLoad() {
+  
+  @IBOutlet weak var priceProduct: UILabel!
+  @IBOutlet weak var propertyPrice: UILabel!
+  @IBOutlet weak var sizeProduct: UILabel!
+  @IBOutlet weak var propertySize: UILabel!
+  @IBOutlet weak var colorProduct: UILabel!
+  @IBOutlet weak var propertyColor: UILabel!
+  @IBOutlet weak var descriptionProduct: UITextView!
+  @IBOutlet weak var nameProduct: UILabel!
+  override func viewDidLoad() {
         super.viewDidLoad()
       title = product?.nameProduct
-      
-
+    descriptionProduct.isEditable = false
+    dateConfiguration()
     }
+  
+  func dateConfiguration (){
+    guard let product = product else {return}
+    nameProduct.text = product.nameProduct
+    descriptionProduct.text = product.description
+    propertyColor.text = "Color:"
+    colorProduct.text = product.colorProduct
+    propertySize.text = "Talle:"
+    sizeProduct.text = product.sizeProduct
+    propertyPrice.text = "Precio:"
+    priceProduct.text = product.priceProduct
+    
+  }
   
 }
